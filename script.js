3db1b6040a772659f766c72a87d03c9de1e6a066
Eric Tao <shuaibtcm@gmail.com>
2025-11-04 19:52:48 +0000

    Reposite for deploy the chatbox seperately

---
+++ script.js
@@ -0,0 +1,42 @@
+ const socket = io('http://localhost:3000')
+ const messageContainer = document.getElementById('message-container')
+ const messageForm = document.getElementById('send-container')
+ const messageInput = document.getElementById('message-input')
+
+
+const name = prompt("What's your name?")
+    appendMessage('You joined')
+socket.emit('new-user', name)
+
+socket.on('chat-message', data => {
+    appendMessage(`${data.name}: ${data.message}`, 'incoming')
+})
+
+socket.on('user-connected', name => {
+    appendMessage(`${name} connected`, 'incoming')
+})
+
+socket.on('user-disconnected', name => {
+    console.log('user-disconnected event received:', name)
+    appendMessage(`${name || 'A user'} disconnected`, 'incoming')
+})
+
+ messageForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const message = messageInput.value
+    appendMessage(`You: ${message}`, 'outgoing')
+    socket.emit('send-chat-message', message) 
+    messageInput.value = ''
+})
+
+function appendMessage(message, type = 'incoming'){
+    const messageElement = document.createElement('div')
+    messageElement.innerText = message
+    messageElement.classList.add('message');
+    messageElement.classList.add(type);  // 'incoming' or 'outgoing'
+    messageContainer.append(messageElement)
+
+    requestAnimationFrame(() => {
+    messageElement.classList.add('show');
+  });
+}
